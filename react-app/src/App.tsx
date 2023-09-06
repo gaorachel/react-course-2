00f476cb526c87da90b8df70831506c1a45063fc
Rachel <gggggrea@gmail.com>
2023-09-06 22:10:47 +0100

    Children props

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import ListGroup from "./components/ListGroup";
+import { Alert } from "./components/Alert";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
   return (
     <div>
-      <ListGroup items={items} heading="Cities" onSelectItem={() => console.log(items)} />
+      <Alert>
+        <span> xxxx </span>
+      </Alert>
     </div>
   );
 }
